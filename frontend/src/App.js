ea1ae4e5b87eda5d12d8ffe8c6cac063380f5e0e
Cristeee <tomoiucris787@gmail.com>
2022-01-10 21:52:34 +0200

    Standalone js scripts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,26 @@ import React from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import {Navbar,Footer, Home, Login, Register, Dashboard, SignaturePad,CovidChestionar} from "./components/index";
 
+class App extends React.Component {
+  componentDidMount () {
 
-function App() {
-  return (
+    const scriptsPath = [
+      process.env.PUBLIC_URL + "js/vendors.bundle.js",
+      process.env.PUBLIC_URL + "js/scripts.bundle.js",
+      process.env.PUBLIC_URL + "js/wizard.js",
+      "https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js",
+      process.env.PUBLIC_URL + "js/load_font.js"
+    ];
+
+    scriptsPath.forEach(element => {
+      const script = document.createElement("script");
+      script.src = element;
+      script.async = true;
+      document.body.appendChild(script);
+    });
+  }
+  render() {
+      return (
     <div className="App">
      <Router>
        
@@ -22,6 +39,8 @@ function App() {
      </Router>
     </div>
   );
+  }
+
 }
 
 export default App;
---
+++ frontend/src/assets/app/js/load_font.js
@@ -0,0 +1,4 @@
+
+alert("loaded");
+
+console.log("asdads");
